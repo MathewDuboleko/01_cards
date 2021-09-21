975747576c2685d75afc9cc29074d12d084084a2
MathewDuboleko <matveyduboleko@gmail.com>
2021-09-21 20:18:46 +0300

    'add: new functionality'

--- cards.js
+++ cards.js
@@ -1,15 +1,21 @@
-const cards = document.querySelectorAll(".card");
+function cardsPlugin(active = 0) {
+  const cards = document.querySelectorAll(".card");
 
-for (const card of cards) {
-  card.addEventListener("click", () => {
-    clearActiveClasses();
+  cards[active].classList.add("active__card");
 
-    card.classList.add("active__card");
-  });
+  for (const card of cards) {
+    card.addEventListener("click", () => {
+      clearActiveClasses();
+
+      card.classList.add("active__card");
+    });
+  }
+
+  function clearActiveClasses() {
+    cards.forEach((card) => {
+      card.classList.remove("active__card");
+    });
+  }
 }
 
-function clearActiveClasses() {
-  cards.forEach((card) => {
-    card.classList.remove("active__card");
-  });
-}
+cardsPlugin(2);
